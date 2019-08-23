2db433338c4663d87edaf36743787872a750d97a
Alex Faissal <alefaissal@gmail.com>
2019-08-23 14:15:07 -0700

    Add css for 404 page and image

--- 404.php
+++ 404.php
@@ -13,15 +13,10 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try a search?' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
+				<div class="four-page">
+				<img src="<?php echo get_home_url(); ?>/wp-content/themes/quotesondev/images/404-quotes.png" alt="">
+				
+			</div>
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
